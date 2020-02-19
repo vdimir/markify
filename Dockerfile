2d57805039d92d4d756aa4d3f24e0a618ef8965a
vdimir <vdimir@users.noreply.github.com>
2020-02-19 11:29:10 +0300

    Add docker stuff

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM golang:1.13-alpine as build-backend
+
+ADD . /build
+WORKDIR /build
+
+RUN go get -v -t -d ./... && \
+    go get github.com/rakyll/statik
+
+# RUN go install -v ./...
+
+RUN GOPATH=$(go env GOPATH) go generate ./... && \
+    version=$(date +%Y%m%d_%H%M%S) && \
+    echo "version=$version" && \
+    go build -o markify -ldflags "-X main.revision=${version} -s -w" ./
+
+
+FROM alpine:3.11
+
+WORKDIR /srv
+
+COPY --from=build-backend /build/markify /srv/markify
+
+EXPOSE 8080
+
+CMD ["/srv/markify"]
--- main.go
+++ main.go
@@ -11,20 +11,18 @@ import (
 
 //go:generate $GOPATH/bin/statik --src=./assets --dest=./ -p app -f
 
-var version = "local"
-var date = "local"
-var commit = "local"
+var revision = "local"
 
 // Opts contains command line options (see go-flags for details)
 type Opts struct {
 	Hostname string `short:"h" long:"host" required:"false" description:"server host name" env:"SERVER_HOSTNAME"`
 	Port     uint16 `short:"p" long:"port" required:"false" description:"server port" env:"SERVER_PORT" default:"8080"`
-	DataDir  string `short:"d" long:"data" required:"false" description:"path to directory with data"`
+	DataDir  string `short:"d" long:"data" required:"false" description:"path to directory with data" env:"DB_DATA_PATH"`
 	Debug    bool   `long:"debug" description:"debug mode"`
 }
 
 func main() {
-	fmt.Printf("Running version %s (%s) -- %s\n", version, commit, date)
+	fmt.Printf("Running version %s\n", revision)
 	var opts Opts
 
 	_, err := flags.Parse(&opts)
@@ -41,11 +39,7 @@ func main() {
 		PageCachePath:  path.Join(opts.DataDir, "page.db"),
 		URLHashPath:    path.Join(opts.DataDir, "url_hash.db"),
 		MdTextPath:     path.Join(opts.DataDir, "mdtext.db"),
-		StatusText: fmt.Sprintf(`{`+
-			`"commit":"%s",`+
-			`"version":"%s",`+
-			`"date":"%s"`+
-			`}`, commit, version, date),
+		StatusText: fmt.Sprintf(`{"revision":"%s"}`, revision),
 	})
 
 	if err != nil {
